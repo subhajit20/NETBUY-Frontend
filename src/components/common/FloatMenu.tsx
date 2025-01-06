72882b13f0e40f5099e15330443dfecfadae8215
Subhajit Ghosh <subhajitstd07@gmail.com>
2025-01-06 21:30:49 +0530

    Modifiying the float menu

--- src/components/common/FloatMenu.tsx
+++ src/components/common/FloatMenu.tsx
@@ -1,7 +1,8 @@
 import { GrHomeRounded } from "react-icons/gr";
 import { IoCartOutline } from "react-icons/io5";
-import { CiUser } from "react-icons/ci";
+import { RiUserLine } from "react-icons/ri";
 import { IoReorderFour } from "react-icons/io5";
+import { NavLink } from "react-router-dom";
 
 
 type Props = {}
@@ -11,10 +12,10 @@ function FloatMenu({}: Props) {
     <div className="fixed w-full bottom-0 px-8 py-2 block sm:hidden">
         <div className="float_menu bg-brandColor py-2 px-10 rounded-full">
             <nav className="flex justify-between items-center w-full text-white">
-                <GrHomeRounded className="w-5 h-5" />
-                <IoCartOutline className="w-6 h-6" />
-                <CiUser className="w-6 h-6" />
-                <IoReorderFour className="w-5 h-5" />
+                <NavLink to="/" className={"btn bg-transparent hover:bg-white hover:text-brandColor text-white"}><GrHomeRounded className="w-5 h-5" /></NavLink>
+                <NavLink to="/" className={"btn bg-transparent hover:bg-white hover:text-brandColor text-white"}><IoCartOutline className="w-6 h-6" /></NavLink>
+                <NavLink to="/" className={"btn bg-transparent hover:bg-white hover:text-brandColor text-white"}><RiUserLine className="w-6 h-6" /></NavLink>
+                <NavLink to="/" className={"btn bg-transparent hover:bg-white hover:text-brandColor text-white"}><IoReorderFour className="w-6 h-6" /></NavLink>
             </nav>
         </div>
     </div>
