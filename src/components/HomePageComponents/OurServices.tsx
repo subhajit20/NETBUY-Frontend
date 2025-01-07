ac7d16719b16345b410e44d679568952f27b9f33
Subhajit Ghosh <subhajitstd07@gmail.com>
2025-01-07 21:22:56 +0530

    Modified the services section

--- src/components/HomePageComponents/OurServices.tsx
+++ src/components/HomePageComponents/OurServices.tsx
@@ -5,15 +5,17 @@ type Props = {}
 function OurServices({}: Props) {
   return (
     <div className="min-h-screen bg-gray-100 flex flex-col items-center p-10" id="ourServices">
-        <h5 className="pt-16 pb-10" id="super_Heading">
+        <h5 className="pt-10 md:pt-16" id="super_Heading">
             <span className="bg-brandColor text-white px-5 py-1 rounded-full">Our Services</span>
         </h5>
-        <h1 className="text-center text-2xl sm:text-4xl md:text-5xl lg:text-7xl">
-            We offer our customers 
-        </h1>
-        <h1 className="text-base sm:text-2xl md:text-4xl lg:text-5xl text-center">
-            a range of amazing benefits
-        </h1>
+        <div className="services_headings pt-10">
+            <h1 className="text-center text-2xl sm:text-4xl md:text-5xl lg:text-7xl">
+                We offer our customers 
+            </h1>
+            <h1 className="text-base sm:text-2xl md:text-4xl lg:text-5xl text-center">
+                a range of amazing benefits
+            </h1>
+        </div>
 
         {/* cards container */}
         <ServiceCardContainer />
